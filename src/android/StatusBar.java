79583ae86385ea7d3108adfb54925eeebcdb3575
Edoardo Tassinari <edotassi@users.noreply.github.com>
2016-12-16 13:12:07 +0100

    Update StatusBar.java

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -153,8 +153,9 @@ public class StatusBar extends CordovaPlugin {
                 window.clearFlags(0x04000000); // SDK 19: WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
                 window.addFlags(0x80000000); // SDK 21: WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 try {
+                    window.setStatusBarColor(Color.parseColor(colorPref));
                     // Using reflection makes sure any 5.0+ device will work without having to compile with SDK level 21
-                    window.getClass().getDeclaredMethod("setStatusBarColor", int.class).invoke(window, Color.parseColor(colorPref));
+                    //window.getClass().getDeclaredMethod("setStatusBarColor", int.class).invoke(window, Color.parseColor(colorPref));
                 } catch (IllegalArgumentException ignore) {
                     LOG.e(TAG, "Invalid hexString argument, use f.i. '#999999'");
                 } catch (Exception ignore) {
